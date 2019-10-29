aa81faa1f6bfd37e3fcd8f456dfea82103194bb9
rmit-s3681278-haoqian-yang <42133486+rmit-s3681278-haoqian-yang@users.noreply.github.com>
2019-10-29 21:50:53 +1100

    update

--- src/service/API.js
+++ src/service/API.js
@@ -4,9 +4,9 @@ export default () => {
     return axios.create({
     //  baseURL: 'http://157.245.63.35:8000/iContinu',
     //    baseURL: 'https://157.245.63.35:8000/iContinu',
-
+        baseURL:'https://www.nathan-yang-api.com/iContinu',
         // baseURL: 'http://157.245.63.35:8001/iContinu',
-        baseURL: '/iContinu',
+        // baseURL: '/iContinu',
         // baseURL: 'http://13.54.207.36:8000/iContinu',
         // baseURL: 'http://127.0.0.1:8000/iContinu',
         timeout: 1000 * 5,
