f110b1be90d4c55cd7f7e0e144ddff4f00a802c5
rmit-s3681278-haoqian-yang <42133486+rmit-s3681278-haoqian-yang@users.noreply.github.com>
2019-10-29 23:35:10 +1100

    update

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+
   devServer: {
     //   open: process.platform === 'darwin',
     // host: '0.0.0.0',
@@ -12,7 +13,9 @@ module.exports = {
     
     proxy:{
       '/iContinu':{ //the middleware of the API address
-        target:'http://127.0.0.1:8000/' // the root API address
+        // target:'http://127.0.0.1:8000/' // the root API address
+        target:'https://www.nathan-yang-api.com/',
+        crossorign: true
         // target: 'http://157.245.63.35:8001/',
       }
     },
